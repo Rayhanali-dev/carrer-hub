b08a803b6c2ebde959fd7ed76f0349ddf58f7a7d
Rayhanali-dev <rayhanalidev@gmail.com>
2023-04-10 00:17:23 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='bg-slate-600 py-8'>
-            <div className='max-w-7xl mx-auto flex justify-between'>
-                <div className="logo">
-                    <h3>Logo</h3>
+            <div className='max-w-7xl mx-auto flex justify-between items-center'>
+                <Link to={`/`}>
+                    <h3 className='text-2xl font-bold text-white'>Job Search Genius</h3>
+                </Link>
+                <div className='flex gap-5'>
+                    <NavLink className="text-white text-[18px] font-medium" to={`/`}>Home</NavLink>
+                    <NavLink className="text-white text-[18px] font-medium" to={`/statistics`}>Statistics</NavLink>
+                    <NavLink className="text-white text-[18px] font-medium" to={`/apply`}>Applied Jobs</NavLink>
+                    <NavLink className="text-white text-[18px] font-medium" to={`/blog`}>Blog</NavLink>
                 </div>
                 <div>
-                    <NavLink to={`/`}>Home</NavLink>
-                </div>
-                <div>
-                    <button>Just akta button</button>
+                    <button className="btn btn-active btn-secondary">Star Applying</button>
                 </div>
             </div>
         </nav>
