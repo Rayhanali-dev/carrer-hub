454ec30615cbcee1c462291d888e2a8709cafd4c
Rayhanali-dev <rayhanalidev@gmail.com>
2023-04-11 14:08:45 +0600

    statistics part added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,8 +2,43 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            
+        <div className='max-w-7xl mx-auto mt-10'>
+            <div className="collapse collapse-arrow mb-8">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    Click me to show/hide content
+                </div>
+                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <p>hello</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow mb-8">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    Click me to show/hide content
+                </div>
+                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <p>hello</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow mb-8">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    Click me to show/hide content
+                </div>
+                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <p>hello</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow mb-8">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    Click me to show/hide content
+                </div>
+                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <p>hello</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,65 @@
 import React from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+
 
 const Statistics = () => {
+    const data = [
+        {
+            id: 1,
+            assignment_name: "HTML CSS & Github",
+            assignment_marks: 60
+        },
+        {
+            id: 2,
+            assignment_name: "Responsive Web Layout",
+            assignment_marks: 60
+        },
+        {
+            id: 3,
+            assignment_name: "CSS Frameworks",
+            assignment_marks: 60
+        },
+        {
+            id: 4,
+            assignment_name: "JavaScript",
+            assignment_marks: 52
+        },
+        {
+            id: 5,
+            assignment_name: "Intregrate JavaScript",
+            assignment_marks: 58
+        },
+        {
+            id: 6,
+            assignment_name: "Api",
+            assignment_marks: 60
+        },
+        {
+            id: 7,
+            assignment_name: "Debugging",
+            assignment_marks: 60
+        }
+    ]
     return (
-        <div>
-            
+        <div className='mx-auto max-w-7xl'>
+            <AreaChart
+                width={1320}
+                height={400}
+                data={data}
+                margin={{
+                    top: 10,
+                    right: 30,
+                    left: 0,
+                    bottom: 0,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="assignment_name" />
+                <YAxis dataKey="assignment_marks" />
+                <Tooltip />
+                <Area type="monotone" dataKey="assignment_name" stroke="#8884d8" fill="#8884d8" />
+                <Area type="monotone" dataKey="assignment_marks" stroke="#8884d8" fill="#8884d8" />
+            </AreaChart>
         </div>
     );
 };
